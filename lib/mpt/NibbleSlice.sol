8ab3d7385d430fc16fa112cb775a6234fcf2f50e
nuel-ikwuoma <emmanuelikwuoma7@gmail.com>
2023-02-17 13:41:15 +0100

    added implementation for NibbleSlice

--- lib/mpt/NibbleSlice.sol
+++ lib/mpt/NibbleSlice.sol
@@ -8,8 +8,12 @@ struct NibbleSlice {
 }
 
 library NibbleSliceOps {
+
+    uint256 constant NIBBLE_PER_BYTE = 2;
+    uint256 constant BITS_PER_NIBBLE = 4;
+
     function len(NibbleSlice memory nibble) public pure returns (uint256) {
-        return nibble.data.length * 2 - nibble.offset;
+        return nibble.data.length * NIBBLE_PER_BYTE - nibble.offset;
     }
 
     function mid(NibbleSlice memory self, uint256 i) public pure returns (NibbleSlice memory) {
@@ -20,18 +24,75 @@ library NibbleSliceOps {
         return len(self) == 0;
     }
 
-    function eq(NibbleSlice memory self, NibbleSlice memory other) public pure returns (bool) {
+    function eq(NibbleSlice calldata self, NibbleSlice calldata other) public pure returns (bool) {
         return len(self) == len(other) && startsWith(self, other);
     }
 
     function at(NibbleSlice memory self, uint256 i) public pure returns (uint256) {
-        //todo: https://github.com/paritytech/trie/blob/0b5b7a54bda23c815c130310a513bdb38251ed12/trie-db/src/nibble/nibbleslice.rs#L109
-        return 0;
+        uint256 ix = (self.offset + i) / NIBBLE_PER_BYTE;
+        uint256 pad = (self.offset + i) % NIBBLE_PER_BYTE;
+        uint8 data = uint8(self.data[ix]);
+        return (pad == 1) ? data & 0x0F : data >> BITS_PER_NIBBLE;
     }
 
-    function startsWith(NibbleSlice memory self, NibbleSlice memory other) public pure returns (bool) {
-        // todo: https://github.com/paritytech/trie/blob/master/trie-db/src/nibble/nibbleslice.rs#L130
-        return false;
+    function startsWith(NibbleSlice calldata self, NibbleSlice calldata other) public pure returns (bool) {
+        return commonPrefix(self, other) == len(other);
+    }
+
+    function commonPrefix(NibbleSlice calldata self, NibbleSlice calldata other) public pure returns (uint256) {
+        uint256 self_align = self.offset % NIBBLE_PER_BYTE;
+        uint256 other_align = other.offset % NIBBLE_PER_BYTE;
+
+        if(self_align == other_align) {
+            uint256 self_start = self.offset / NIBBLE_PER_BYTE;
+            uint256 other_start = other.offset / NIBBLE_PER_BYTE;
+            uint256 first = 0;
+
+            if(self_align != 0) {
+                if((self.data[self_start] & 0x0F) != (other.data[other_start] & 0x0F)) {
+                    return 0;
+                }
+                ++self_start;
+                ++other_start;
+                ++first;
+            }
+            return biggestDepth(self.data[self_start:], other.data[other_start:]) + first;
+        }else {
+            uint256 s = min(len(self), len(other));
+            uint256 i = 0;
+            while(i < s) {
+                if(at(self,i) != at(other, i)) {
+                    break;
+                }
+                ++i;
+            }
+            return i;
+        }
+    }
+
+    function biggestDepth(bytes calldata a, bytes calldata b) public pure returns (uint256) {
+        uint256 upperBound = min(a.length, b.length);
+        uint256 i = 0;
+        while(i < upperBound) {
+            if(a[i] != b[i]) {
+                return i * NIBBLE_PER_BYTE + leftCommon(a[i], b[i]);
+            }
+            ++i;
+        }
+        return i * NIBBLE_PER_BYTE;
+    }
+
+    function leftCommon(bytes1 a, bytes1 b) public pure returns (uint256) {
+        if (a == b) {
+            return 2;
+        }else if(uint8(a) & 0xF0 == uint8(b) & 0xF0) {
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+
+    function min(uint256 a, uint256 b) private pure returns (uint256) {
+        return (a < b) ? a : b;
     }
 }
-
