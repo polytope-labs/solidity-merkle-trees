8a01294f781f2cae9acbb041566abcc504f4fbb8
Seun Lanlege <seunlanlege@gmail.com>
2024-09-13 11:06:23 +0100

    downgrade to v0.8.17

--- src/MerklePatricia.sol
+++ src/MerklePatricia.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.20;
+pragma solidity 0.8.17;
 
 import "./trie/Node.sol";
 import "./trie/Option.sol";
--- src/trie/ethereum/EthereumTrieDB.sol
+++ src/trie/ethereum/EthereumTrieDB.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.20;
+pragma solidity 0.8.17;
 
 import "../Node.sol";
 import "../Bytes.sol";
