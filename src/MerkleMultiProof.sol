68edb4cad404d42b2377ff6c59d789d8d3d2e34d
Seun Lanlege <seun@polytope.technology>
2025-12-22 18:49:42 +0000

    update openzeppelin

--- src/MerkleMultiProof.sol
+++ src/MerkleMultiProof.sol
@@ -1,5 +1,18 @@
-// SPDX-License-Identifier: Apache2
-pragma solidity ^0.8.17;
+// Copyright (C) Polytope Labs Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/utils/math/Math.sol";
 import "./Types.sol";
@@ -98,7 +111,7 @@ library MerkleMultiProof {
         uint256 leavesCount
     ) internal pure returns (bytes32) {
         // Calculate tree height
-        uint256 height = Math.log2(leavesCount, Math.Rounding.Up);
+        uint256 height = Math.log2(leavesCount, Math.Rounding.Ceil);
 
         // Initialize tracking variables
         uint256 p; // proof index
