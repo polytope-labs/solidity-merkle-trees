03832eb448ab77e5010281d7894c77b92a0640ad
Seun Lanlege <seun@polytope.technology>
2025-12-23 12:47:32 +0100

    update openzeppelin (#47)
    * update openzeppelin
    
    * download svm release list manually
    
    * --test-threads=1

--- src/MerklePatricia.sol
+++ src/MerklePatricia.sol
@@ -1,4 +1,18 @@
-pragma solidity ^0.8.17;
+// Copyright (C) Polytope Labs Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+pragma solidity ^0.8.20;
 
 import "./trie/Node.sol";
 import "./trie/Option.sol";
@@ -8,8 +22,6 @@ import "./trie/TrieDB.sol";
 import "./trie/substrate/SubstrateTrieDB.sol";
 import "./trie/ethereum/EthereumTrieDB.sol";
 import "./Types.sol";
-
-// SPDX-License-Identifier: Apache2
 
 /**
  * @title A Merkle Patricia library
